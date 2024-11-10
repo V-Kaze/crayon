9a386c863742fea7c9889691ce9556c4c5cbf896
Asapin <185166148+V-Kaze@users.noreply.github.com>
2024-11-10 23:03:57 +0900

    Fixed color cell renderer

--- src/main/java/io/github/vkaze/crayon/storage/Crayon.java
+++ src/main/java/io/github/vkaze/crayon/storage/Crayon.java
@@ -2,6 +2,10 @@ package io.github.vkaze.crayon.storage;
 
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.JBColor;
+import com.intellij.util.ui.ColorIcon;
+import com.intellij.util.ui.EmptyIcon;
+
+import java.awt.Color;
 
 @SuppressWarnings("ImmutableEnumChecker")
 public enum Crayon {
@@ -11,16 +15,16 @@ public enum Crayon {
     ROSE(new JBColor(0xf2dcda, 0x6e535b)),
     VIOLET(new JBColor(0xe6e0f1, 0x534a57)),
     YELLOW(new JBColor(0xffffe4, 0x4f4b41));
-    private final JBColor color;
+    private final ColorIcon colorIcon;
     private final String colorName;
 
-    Crayon(JBColor color) {
-        this.color = color;
+    Crayon(Color color) {
+        this.colorIcon = new ColorIcon(EmptyIcon.ICON_16.getIconWidth(), color);
         this.colorName = StringUtil.capitalize(name().toLowerCase());
     }
 
-    public JBColor getColor() {
-        return color;
+    public ColorIcon getColorIcon() {
+        return colorIcon;
     }
 
     public String getColorName() {
--- src/main/java/io/github/vkaze/crayon/ui/table/ColorCellRenderer.java
+++ src/main/java/io/github/vkaze/crayon/ui/table/ColorCellRenderer.java
@@ -1,51 +1,25 @@
 package io.github.vkaze.crayon.ui.table;
 
-import com.intellij.ui.JBColor;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.ui.EmptyIcon;
 import io.github.vkaze.crayon.storage.Crayon;
-import org.jspecify.annotations.Nullable;
 
-import javax.swing.Icon;
-import javax.swing.JLabel;
-import javax.swing.JTable;
-import javax.swing.table.TableCellRenderer;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Graphics;
+import javax.swing.table.DefaultTableCellRenderer;
 
-public class ColorCellRenderer extends JLabel implements TableCellRenderer {
-    @Nullable
-    private JBColor iconColor;
+public class ColorCellRenderer extends DefaultTableCellRenderer {
+    private static final Logger log = Logger.getInstance(ColorCellRenderer.class);
 
     public ColorCellRenderer() {
-        setOpaque(true);
         setIcon(EmptyIcon.ICON_16);
     }
 
     @Override
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+    protected void setValue(Object value) {
         if (value instanceof Crayon crayon) {
-            iconColor = crayon.getColor();
-            setText(crayon.getColorName());
-        }
-        return this;
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        if (iconColor != null) {
-            final Icon icon = getIcon();
-            final int width = icon.getIconWidth();
-            final int height = icon.getIconHeight();
-
-            final Color old = g.getColor();
-
-            g.setColor(iconColor);
-            g.fillRect(0, 0, width, height);
-
-            g.setColor(old);
+            log.warn("Creating new color icon. Current object: " + this.hashCode());
+            setIcon(crayon.getColorIcon());
+        } else {
+            super.setValue("The value is not a Crayon");
         }
     }
 }
